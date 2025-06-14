5efa7f95af6b0cc1b63e30466597cd670672319f
Mario Alberto Brenes Trejos <mario.brenestrejos@estudiantec.cr>
2025-06-14 23:37:31 +0000

    laboratorio 4 finalizado mariobtlab04.surge.sh

--- Laboratorio 4/laboratorio4/src/App.vue
+++ Laboratorio 4/laboratorio4/src/App.vue
@@ -1,24 +1,15 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import SideNav from './components/SideNav.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <SideNav />
+  <div class="container-fluid p-0">
+    <RouterView />
+  </div>
 </template>
+
 
 <style scoped>
 header {
--- Laboratorio 4/laboratorio4/src/router/index.ts
+++ Laboratorio 4/laboratorio4/src/router/index.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/about'
     },
     {
       path: '/about',
@@ -15,7 +13,32 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/About.vue'),
+    },
+    {
+      path: '/experience',
+      name: 'experience',
+      component: () => import('../views/Experience.vue'),
+    },
+        {
+      path: '/education',
+      name: 'education',
+      component: () => import('../views/Education.vue'),
+    },
+    {
+      path: '/awards',
+      name: 'awards',
+      component: () => import('../views/Awards.vue'),
+    },
+    {
+      path: '/interests',
+      name: 'interests',
+      component: () => import('../views/Interests.vue'),
+    },
+    {
+      path: '/skills',
+      name: 'skills',
+      component: () => import('../views/Skills.vue'),
     },
   ],
 })
